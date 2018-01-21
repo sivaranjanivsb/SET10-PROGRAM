44948d4f9ad0247e0b4bf038434420ffd47d47e7
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-01-21 16:47:09 +0530

    Create TO ADD DIGITS IN A NUMBER.C

---
+++ TO ADD DIGITS IN A NUMBER.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	 int no,rem,sum=0;
+	 scanf("%d",&no);
+	 while(no!=0)
+	 {
+	 	rem=no%10;
+	 	sum=sum+rem;
+	 	no=no/10;
+	 }
+	 printf("%d",sum);
+	 return 0;
+}
